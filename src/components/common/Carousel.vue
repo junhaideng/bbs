9faa0e0b1018258877d7acfbb7467813f510acf5
Cyberist-Edgar <201648748@qq.com>
2020-05-20 23:57:41 +0800

    modify style

--- src/components/common/Carousel.vue
+++ src/components/common/Carousel.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <a-carousel autoplay>
+    <a-carousel autoplay arrows>
+       <div
+      slot="prevArrow"
+      class="custom-slick-arrow"
+      style="left: 10px;zIndex: 1"
+    >
+      <a-icon type="left-circle" />
+    </div>
+    <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px">
+      <a-icon type="right-circle" />
+    </div>
       <a-row v-for="(id, index) in id_list" :key="index">
  <div>
           <img
@@ -45,6 +55,28 @@ export default {
 <style scoped>
 .ant-carousel >>> .slick-slide {
   text-align: center;
+  height: 400px;
+  line-height: 160px;
+  background: #364d79;
   overflow: hidden;
+}
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+}
+
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel >>> .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
 }
 </style>
